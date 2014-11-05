df05e689724c037e25f56703a1e91de6cd316d3c
jking31 <james.kingiii@gtri.gatech.edu>
2014-11-05 15:02:15 -0500

    Init commit with library and build working.
    Currently, this only shows a PApplet on screen, so changes still need to be made.

---
+++ src/MyApplet.java
@@ -0,0 +1,12 @@
+import processing.core.PApplet;
+
+
+public class MyApplet extends PApplet {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		PApplet.main("MyApplet");
+	}
+
+}
